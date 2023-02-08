13202e56307a9d7810685796941cabbddcd563fd
Gustavo Costa Neves <gustavo.neves@thoughtworks.com>
2023-02-08 14:24:56 -0300

    fix

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(jsonServer.bodyParser)
-server.use(jsonServer.defaults)
 
 server.post('/todo', (req, res) => {
     const db = router.db.getState().todo
